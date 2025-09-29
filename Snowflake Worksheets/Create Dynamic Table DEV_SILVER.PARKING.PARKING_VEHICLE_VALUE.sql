2e7bd7548f77d9e7336701b4c5d4d091f2ae2d76
Ajlee-BI <ajlee@san.org>
2025-09-29 14:47:18 -0700

    Create Dynamic Table DEV_SILVER.PARKING.PARKING_VEHICLE_VALUE.sql

--- Snowflake Worksheets/Create Dynamic Table DEV_SILVER.PARKING.PARKING_VEHICLE_VALUE.sql
+++ Snowflake Worksheets/Create Dynamic Table DEV_SILVER.PARKING.PARKING_VEHICLE_VALUE.sql
@@ -11,7 +11,7 @@ create or replace dynamic table DEV_SILVER.PARKING.PARKING_VEHICLE_VALUE(
 ) target_lag = '1 hour' refresh_mode = AUTO initialize = ON_CREATE warehouse = SAN_DEV_SERVICE_ELT_WH
  as
 SELECT DISTINCT
-    UPPER(TRIM(PLATE))                     AS LICENSE_PLATE_NUMBER,
+    UPPER(TRIM(PLATE))                     AS LICENSE_PLATE_NUMBER, /*UPPER(TRIM)) This capitlizes everything and removes any trailing or leading spaces*/
     UPPER(TRIM(STATE))                     AS STATE,
     UPPER(TRIM("TRIM"))                    AS TRIM_LEVEL,
     UPPER(TRIM(VIN))                       AS VIN,
@@ -21,4 +21,5 @@ SELECT DISTINCT
     CLEAN_MARKET_VALUE,
     IMPORT_DATE
 FROM DEV_BRONZE.PARKING.VEHICLE_DATABASES_API_RESULTS_DATA
+
 WHERE CLEAN_MARKET_VALUE IS NOT NULL
